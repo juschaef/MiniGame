6ec109c1497536adb3ef3247006d61499d12382d
juschaef <juschaef@student.42.fr>
2015-01-22 20:19:07 +0100

    pong: unuse value & readme update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MiniGame
+
+Shell Minigame in C
+
+Pong :	- bresenham algorithm for ball movement.
+		- Missing angle handling for collide to wall.
--- pong/pong_init.c
+++ pong/pong_init.c
@@ -10,8 +10,6 @@ t_pong  *pong_init(void)
 	pong->p2_pos.x = 56;
 	pong->p2_pos.y = 9;
 	pong->b_orig.x = 30;
-	pong->b_save.x = pong->b_orig.x + 50;
-	pong->b_save.y = pong->b_orig.y + 50;
 	pong->b_orig.y = 3;
 	pong->b_dest.x = 0;
 	pong->b_dest.y = 0;
